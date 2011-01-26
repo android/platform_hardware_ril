d7f3ba87b3510eba3bd6b17cc6ab9af511be94e0
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-01-26 17:13:56 +0800

    mock-ril: fix compiling an issue on Mac OS X
    The $(src_java) and $(gen_src_java) are empty in mock-ril/Android.mk
    if the architecuture is not arm, or the build is non-debug variants.
    It results in a "find  -name ..." statement with an empty searching
    directory by $(call all-java-files-under,...) of librilproto-java.
    The BSD derived find on Mac OS X complains about that.
    
    The patch tried to fix the issue in the ways:
    
    * always defines $(src_java) and $(gen_src_java) no matter what TARGET_ARCH is.
    * only defines module librilproto-java in debug variants build.
    
    Change-Id: I8a04657d280abc8df1fc3cf95d7143c2225f6ef5
    
    Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

--- mock-ril/Android.mk
+++ mock-ril/Android.mk
@@ -4,12 +4,8 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(TARGET_ARCH),arm)
-
 # Mock-ril only buid for debug variants
 ifneq ($(filter userdebug eng tests, $(TARGET_BUILD_VARIANT)),)
-
-include $(CLEAR_VARS)
 
 # Directories of source files
 src_cpp=src/cpp
@@ -24,6 +20,10 @@ gen_src_cpp=$(src_generated)/cpp
 gen_src_java=$(src_generated)/java
 gen_src_py=$(src_generated)/python
 gen_src_desc=$(src_generated)/desc
+
+ifeq ($(TARGET_ARCH),arm)
+
+include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
     $(src_cpp)/ctrl_server.cpp \
@@ -76,9 +76,7 @@ LOCAL_MODULE:= libmock_ril
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif
-
-endif
+endif # TARGET_ARCH == arm
 
 # Java librilproto
 # =======================================================
@@ -93,3 +91,5 @@ LOCAL_SRC_FILES := $(call all-java-files-under, $(src_java) $(gen_src_java))
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
 # =======================================================
+
+endif # TARGET_BUILD_VARIANT
