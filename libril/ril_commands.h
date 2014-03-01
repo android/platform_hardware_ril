70ebe1a60f207e70fcff79eae092a6873f97e2ff
Pierre Couillaud <pierre@broadcom.com>
2014-02-28 22:05:05 -0800

    WIP: hardware configuration reporting.
    install unsolicited response from ril allowing to pass
    real time information about the hardware configuration to
    the telephony framework.
    
    Change-Id: I3b2edbd47404c72607b2d9ff0c6571800a41fdd5
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- libril/ril_commands.h
+++ libril/ril_commands.h
@@ -132,3 +132,4 @@
     {RIL_REQUEST_SIM_OPEN_CHANNEL, dispatchString, responseInts},
     {RIL_REQUEST_SIM_CLOSE_CHANNEL, dispatchInts, responseVoid},
     {RIL_REQUEST_SIM_TRANSMIT_APDU_CHANNEL, dispatchSIM_APDU, responseSIM_IO},
+    {RIL_REQUEST_GET_HARDWARE_CONFIG, dispatchVoid, responseHardwareConfig},
