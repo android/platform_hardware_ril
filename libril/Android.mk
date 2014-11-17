521fdf79e5078a295858ab71448686ce33a7d307
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2014-11-17 20:55:46 +0100

    Put the compiler into C++11 mode
    ril.cpp uses C++11 style initializers. Clang refuses to compile it
    unless it is put into C++11 mode. (gcc doesn't care because it
    implements C++11 initializers as a C++98 extension)
    
    Change-Id: I1852f78e60afa268eca66d160737a798a54d70a4
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- libril/Android.mk
+++ libril/Android.mk
@@ -3,6 +3,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_CFLAGS := -std=gnu++11
+
 LOCAL_SRC_FILES:= \
     ril.cpp \
     ril_event.cpp
@@ -15,7 +17,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhardware_legacy \
     librilutils
 
-#LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
+#LOCAL_CFLAGS += -DANDROID_MULTI_SIM -DDSDA_RILD1
 
 ifeq ($(SIM_COUNT), 2)
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
