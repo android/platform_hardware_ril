8c9609cb9d6bf2f0f6fce66c447b6d9b6ec68a8c
Amit Mahajan <amitmahajan@google.com>
2016-08-25 11:19:21 -0700

    Adding simple radio service and callbacks.
    Test: WIP
    Bug: 32020264
    Change-Id: Ib2b3abb54084a1b48c5fd74042fe534e3d9fb69f

--- libril/Android.mk
+++ libril/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES:= \
     ril_event.cpp\
     RilSocket.cpp \
     RilSapSocket.cpp \
+    ril_service.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
@@ -16,6 +17,9 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libhardware_legacy \
     librilutils \
+    android.hardware.radio@1.0 \
+    libhidl  \
+    libhwbinder
 
 LOCAL_STATIC_LIBRARIES := \
     libprotobuf-c-nano-enable_malloc \
