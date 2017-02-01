CyanogenMod/android_hardware_ril

Add p2 option in openLogicalChannel
- Adding support for P2 parameter
- Needed to support OMAPI 3.0 defined
by SIM Alliance

Change-Id: I9679178e2a6f0decd48f30e358685106b91163b8
CRs-Fixed: 948787

libril/ril_commands.h
@@ -151,3 +151,4 @@
     {RIL_REQUEST_PULL_LCEDATA, dispatchVoid, responseLceData},
     {RIL_REQUEST_GET_ACTIVITY_INFO, dispatchVoid, responseActivityData},
     {RIL_REQUEST_SIM_GET_ATR, dispatchInts, responseString},
+    {RIL_REQUEST_CAF_SIM_OPEN_CHANNEL_WITH_P2, dispatchOpenChannelWithP2, responseInts},

android/platform_hardware_ril

Bug: 27360148

libril/ril_commands.h
@@ -129,7 +129,7 @@
     {RIL_REQUEST_IMS_REGISTRATION_STATE, dispatchVoid, responseInts},
     {RIL_REQUEST_IMS_SEND_SMS, dispatchImsSms, responseSMS},
     {RIL_REQUEST_SIM_TRANSMIT_APDU_BASIC, dispatchSIM_APDU, responseSIM_IO},
-    {RIL_REQUEST_SIM_OPEN_CHANNEL, dispatchString, responseInts},
+    {RIL_REQUEST_SIM_OPEN_CHANNEL, dispatchOpenChannelWithP2, responseInts},
     {RIL_REQUEST_SIM_CLOSE_CHANNEL, dispatchInts, responseVoid},
     {RIL_REQUEST_SIM_TRANSMIT_APDU_CHANNEL, dispatchSIM_APDU, responseSIM_IO},
     {RIL_REQUEST_NV_READ_ITEM, dispatchNVReadItem, responseString},