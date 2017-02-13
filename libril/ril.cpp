b34a5881ee6fe527432dd4d1f6fc01a9523b707f
Jack Yu <jackyu@google.com>
2017-02-13 11:21:00 -0800

    Data call refactoring and hidlization
    Support the new HIDL interface for data call APIs.
    The affacted APIs are RIL_REQUEST_SETUP_DATA_CALL,
    RIL_REQUEST_SET_DATA_PROFILE, RIL_REQUEST_SET_INITIAL_ATTACH_APN
    More parameters are passing down to the modem to address
    some issues we had earlier.
    
    This also backward compatible with the old vendor RIL.
    
    bug: 32224135, 30173958, 33561503, 30282096, 32020264
    Test: Telephony sanity tests, unit tests
    
    Change-Id: I5ed76321ef52d6bc7a00bce918fde7bb316f8c93

--- libril/ril.cpp
+++ libril/ril.cpp
@@ -4517,10 +4517,7 @@ RIL_onRequestAck(RIL_Token t) {
         // to hidl supported apis. They are left out as they use common functions used by
         // unsolicited commands present in other CL
         int cmdId = pRI->pCI->requestNumber;
-        if(cmdId != RIL_REQUEST_SETUP_DATA_CALL
-                && cmdId != RIL_REQUEST_OEM_HOOK_RAW
-                && cmdId != RIL_REQUEST_SET_INITIAL_ATTACH_APN
-                && cmdId != RIL_REQUEST_SET_DATA_PROFILE) {
+        if(cmdId != RIL_REQUEST_OEM_HOOK_RAW) {
             pthread_rwlock_t *radioServiceRwlockPtr = radio::getRadioServiceRwlock(
                     (int) socket_id);
             int rwlockRet = pthread_rwlock_rdlock(radioServiceRwlockPtr);
@@ -4594,10 +4591,7 @@ RIL_onRequestComplete(RIL_Token t, RIL_Errno e, void *response, size_t responsel
 
         bool hidlized = false;
         int cmdId = pRI->pCI->requestNumber;
-        if(cmdId != RIL_REQUEST_SETUP_DATA_CALL
-                && cmdId != RIL_REQUEST_OEM_HOOK_RAW
-                && cmdId != RIL_REQUEST_SET_INITIAL_ATTACH_APN
-                && cmdId != RIL_REQUEST_SET_DATA_PROFILE) {
+        if(cmdId != RIL_REQUEST_OEM_HOOK_RAW) {
             hidlized = true;
         }
 
